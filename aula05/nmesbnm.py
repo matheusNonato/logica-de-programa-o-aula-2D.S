481cc4bd018663a72d14096d60b392889b1a9744
MatheusNonato <matheusnonato318@gmail.com>
2026-04-30 12:17:08 -0300

    correção

---
+++ aula05/nmesbnm.py
@@ -0,0 +1,30 @@
+#nmesbnm
+nome1 = input("digite o primeiro nome completo: ")
+nome2 = input("digite o segundo nome completo: ")
+
+#separando os dois
+parte1 = nome1.split()
+parte2 = nome2.split()
+
+#separando os nomes dentro das variaveis
+primeiro_nome = parte1 [0]
+sobrenome1 = parte2[-1]
+
+segundo_nome = parte2[0]
+sobrenome1 = parte1[-1]
+
+novo_nome1 = primeiro_nome + " " + sobrenome1
+novo_nome2 = segundo_nome + " " + sobrenome2
+
+#separando o primeiro nome e sobrenome 
+primeiro_nome = parte1[0]
+sobrenome1 = parte2[-1]
+
+segundo_nome2 = parte2[0]
+sobrenome2 = parte2[-1]
+
+novo_nome1 = primeiro_nome + " " + sobrenome1
+novo_nome2 = segundo_nome + " " + sobrenome2 
+
+print(novo_nome1)
+print(novo_nome2)
